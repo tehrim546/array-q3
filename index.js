340abee8ce3d7aaa9cf29fe329084ea1e87eb934
= <=>
2024-01-28 10:12:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+//Concatenate two arrays without modifying the original arrays. You can use the + operator for concatenation.
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const concatenatedArray = array1.concat(array2);
+console.log("Original Array 1:", array1);
+console.log("Original Array 2:", array2);
+console.log("Concatenated Array:", concatenatedArray);
